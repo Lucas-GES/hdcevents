dc2cb3247a768fbb0d13378bacbe7c6c9e2437c8
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2024-11-20 22:29:34 -0300

    Avançando na template engine Blade(loop)

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $nome = "Luck";
     $idade = 27;
-    $profissao = 'Programador';
-    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => $profissao]);
+
+    $arr = [10,20,30,40,50];
+
+    $nomes = ['Matheus', 'Maria', 'João', 'Saulo'];
+
+    return view('welcome', 
+    [
+        'nome' => $nome, 
+        'idade' => $idade, 
+        'profissao' => 'Programador',
+        'arr' => $arr,
+        'nomes' => $nomes
+    ]);
 });
 
 Route::get('/contact', function () {
