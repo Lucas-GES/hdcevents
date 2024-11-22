194f254cbe4521cd03c5482b0e7cb432c55ac100
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2024-11-21 23:00:00 -0300

    Resgatando parâmetros de URL

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,6 @@
     <!-- CSS Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-
     <!-- CSS da aplicação -->
     <link rel="stylesheet" href="/css/styles.css">
 
@@ -24,10 +23,40 @@
 </head>
 
 <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="nav-link">
+                        <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
     @yield('content')
     <footer>
         <p>HDC Events &copy; 2024</p>
     </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,11 @@ Route::get('/contact', function () {
 });
 
 Route::get('/produtos', function () {
-    return view('product');
+    $busca = request('search');
+
+    return view('products', ['busca' => $busca]);
+});
+
+Route::get('/produtos_teste/{id?}', function ($id = null) {
+    return view('product', ['id' => $id]);
 });
